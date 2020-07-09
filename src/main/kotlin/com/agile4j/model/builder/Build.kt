cb18c5bfd26bd8a5726502097259219a1b264018
liurenpeng <liurenpeng@kuaishou.com>
2020-07-09 17:29:09 +0800

    isTargetClass

--- src/main/kotlin/com/agile4j/model/builder/Build.kt
+++ src/main/kotlin/com/agile4j/model/builder/Build.kt
@@ -4,8 +4,6 @@ import com.agile4j.model.builder.build.BuildMultiPair
 import com.agile4j.model.builder.build.BuildSinglePair
 import com.agile4j.model.builder.build.ModelBuilder
 import com.agile4j.model.builder.build.buildTargets
-import com.agile4j.model.builder.build.injectModelBuilder
-import com.agile4j.model.builder.build.injectRelation
 import com.agile4j.model.builder.build.modelBuilder
 import com.agile4j.model.builder.build.targetClazz
 import com.agile4j.utils.util.CollectionUtil
@@ -27,22 +25,13 @@ infix fun <T : Any> ModelBuilder.buildMulti(clazz: KClass<T>) =
  * @param T target
  * @param IOA accompanyIndex or accompany
  */
-infix fun <T : Any, IOA> BuildSinglePair<KClass<T>>.by(source: IOA): T? {
-    val coll = this.modelBuilder buildMulti this.targetClazz by singleton(source)
-    return if (CollectionUtil.isEmpty(coll)) null else coll.toList()[0]
-}
+infix fun <T : Any, IOA> BuildSinglePair<KClass<T>>.by(source: IOA): T? =
+    (this.modelBuilder buildMulti this.targetClazz by singleton(source)).firstOrNull()
 
 /**
  * @param T target
  * @param IOA accompanyIndex or accompany
  */
-infix fun <T : Any, IOA> BuildMultiPair<KClass<T>>.by(sources: Collection<IOA>) : Collection<T> {
-    if (CollectionUtil.isEmpty(sources)) {
-        return emptyList()
-    }
+infix fun <T : Any, IOA> BuildMultiPair<KClass<T>>.by(sources: Collection<IOA>) : Collection<T> =
+    if (CollectionUtil.isEmpty(sources)) emptyList() else buildTargets(this, sources)
 
-    val targets = buildTargets(this, sources)
-    injectModelBuilder(this, targets)
-    injectRelation(targets)
-    return targets
-}
